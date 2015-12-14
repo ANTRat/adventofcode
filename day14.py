386ea6a333d762aea9d71525346389c8ebe1a0bd
Jeremy Atkins <antrat@gmail.com>
2015-12-14 08:07:14 -0500

    Day 14: list of speeds instead of 'moving'+speed

--- day14.py
+++ day14.py
@@ -18,17 +18,16 @@ for line in data:
         speed = int(rg[1])
         dur = int(rg[2])
         wait = int(rg[3])
-        moving = []
+        speeds = []
         for a in range(dur):
-            moving.append(True)
+            speeds.append(speed)
         for b in range(wait):
-            moving.append(False)
-        reindeer[name] = {'speed': speed, 'moves': moving}
+            speeds.append(0)
+        reindeer[name] = speeds
 
 for second in range(2503):
     for name, rd in reindeer.items():
-        if rd['moves'][second % len(rd['moves'])]:
-            distances[name] += rd['speed']
+        distances[name] += rd[second % len(rd)]
     for n in distances:
         if distances[n] == max(distances.values()):
             points[n] += 1
